f6a25d40371076d0c9475e078b17fd2b24fa13a2
고정우 <jwko01@naver.com>
2026-06-27 15:18:27 +0900

    프론트엔드 Vite 초기 세팅

---
+++ hackathon-app/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>해커톤 웹사이트</h1>
+      <p>React로 만든 첫 화면입니다.</p>
+      <button>시작하기</button>
+    </div>
+  );
+}
+
+export default App;
